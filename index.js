effc2dd076a8c8c6c2d8d691c2ad43b112774855
Jeff Keen <jeff@keen.me>
2019-11-12 02:03:42 -0600

    Simplify configuration for now

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
   'use strict';
-  const Funnel = require('broccoli-funnel');
-  const mergeTrees = require('broccoli-merge-trees');
-  const join = require('path').join;
-
   module.exports = {
     name: require('./package').name,
-
-    treeForPublic: function() {
-      const imagesDir = new Funnel(join(this.root, 'public'), {
-        include: ['**/*'],
-        destDir: 'public'
-      });
-
-      return mergeTrees([imagesDir]);
+    options: {
+      svgJar: {
+        sourceDirs: [
+          'public',
+          'tests/dummy/public/assets/images/',
+          'node_modules/nypr-icons/public',
+        ],
+      },
     },
-
-    included: function(app) {
-       let { svgJar = {} } = app.options;
-       if (!svgJar.sourceDirs) {
-         svgJar.sourceDirs = [];
-       }
-
-       svgJar.sourceDirs.push('./public/images/icons');
-       this._super.included.apply(this, arguments);
-     },
-
     isDevelopingAddon: function() {
       return true;
     }
